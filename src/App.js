dca6814f20da26482f0f30c5481fde36f463cf05
iqbalreact <iqbalmobiledev@gmail.com>
2020-05-02 14:45:00 +0700

    react hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 
-class App extends React.Component {
-  state = {
-    persons: [
-      { name: 'Felix', age: 24 },
-      { name: 'Ikhwan', age: 26 },
-      { name: 'Iqbal', age: 23 }
-    ],
-    otherState: 'some other value'
-  }
+const App = props => {
+  const [ personsState, setPersonsState ] = useState({
+      persons: [
+        { name: 'Felix', age: 24 },
+        { name: 'Ikhwan', age: 26 },
+        { name: 'Iqbal', age: 23 }
+      ],
+      otherState: 'some other value'
+  });
 
-
-  switchNameHandler = () => {
+  const[otherState, setOtherState] = useState('some other value');
+  console.log(personsState, otherState);
+  const switchNameHandler = () => {
     // console.log('Was clicked!');
     // DON'T DO THIS : this.state.persons[0].name = 'Muhammad Iqbal';
-    this.setState(
+    setPersonsState(
       {
         persons: [
           { name: 'Ayu', age: 24 },
@@ -29,36 +30,35 @@ class App extends React.Component {
       }
     )
   }
-
-  render () {
-    return (
-      <div className ="App">
-        <h1>Hi, This My React App</h1>
-        <p>Really Good !</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
-    );
-  }
+  
+  return (
+    <div className ="App">
+      <h1>Hi, This My React App</h1>
+      <p>Really Good !</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
 }
 
 
 export default App;
+
